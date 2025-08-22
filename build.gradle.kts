96087f65b4c4276d881278549eeabd9bf4eed173
João Pedro <jparaujocle@gmail.com>
2025-08-22 11:50:59 -0300

    chore: few tweaks on CommonsRepository

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    application
 }
 
 configurations {
@@ -11,13 +12,26 @@ configurations {
 group = "br.com.dio_ntt"
 version = "1.0-SNAPSHOT"
 
+application {
+    mainClass.set("br.com.dio_ntt.Main")
+    applicationDefaultJvmArgs = listOf("-Dfile.encoding=UTF-8")
+}
+
+tasks.named<JavaExec>("run") {
+    standardInput = System.`in`
+}
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    compileOnly("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok:1.18.36")
+    compileOnly("org.projectlombok:lombok:1.18.30")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
+
+    testCompileOnly("org.projectlombok:lombok:1.18.30")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.30")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
--- src/main/java/br/com/dio_ntt/repository/CommonsRepository.java
+++ src/main/java/br/com/dio_ntt/repository/CommonsRepository.java
@@ -2,17 +2,16 @@ package br.com.dio_ntt.repository;
 
 import br.com.dio_ntt.exception.NoFundsEnoughException;
 import br.com.dio_ntt.model.Wallet;
-import lombok.NoArgsConstructor;
 
-import static lombok.AccessLevel.PRIVATE;
-
-@NoArgsConstructor(access = PRIVATE)
 public final class CommonsRepository {
+
+    private CommonsRepository() {
+        // Construtor privado
+    }
 
     public static void checkFundsForTransaction(final Wallet source, final long amount) {
         if (source.getFunds() < amount) {
             throw new NoFundsEnoughException("Sua conta não tem dinheiro o suficiente para realizar essa transação");
         }
     }
-
 }
